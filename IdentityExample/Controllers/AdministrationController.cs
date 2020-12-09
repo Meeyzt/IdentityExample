70ca0b6885dc0c323e841ddc0c5a79e1b7d70e36
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-09 21:23:17 +0300

    CreateRole

---
+++ IdentityExample/Controllers/AdministrationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityExample.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}
